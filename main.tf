96c859b0c6a0ebd197aff61e86ffb03e55d7f444
mozy okubo <mozy.okubo@gmail.com>
2020-04-23 23:58:25 +0900

    fix typo

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
   region     = "ap-northeast-1"
 }
 
-resource "aws_vpc" "test_vpc" {
+resource "aws_vpc" "test-vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "test-vpc"
