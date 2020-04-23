26276ab7d1fe0d23bb5125e0c2b87418bf591327
mozy okubo <mozy.okubo@gmail.com>
2020-04-23 23:11:16 +0900

    fix typo

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ variable "aws_secret_key" {}
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  regoin = "ap-northeast-1"
+  regoin     = "ap-northeast-1"
 }
 
-resouce "aws_vpc" "test_vpc" {
+resource "aws_vpc" "test_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "test-vpc"
